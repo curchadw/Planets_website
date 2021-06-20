107da10713b668d6dfe90a0d039b0fe5dd958608
curtis <laurencechadwell@gmail.com>
2021-06-19 22:32:47 -0500

    Updated styling

--- js/script.js
+++ js/script.js
@@ -28,3 +28,41 @@ const earthImgRevert = () =>{
     let earth = document.getElementById('earth')
     earth.src = 'assets/planet-earth.svg' 
 }
+
+const hide_para2_3 = () =>{
+    let para1 = document.getElementById('para1')
+    let para2 = document.getElementById('para2')
+    let para3 = document.getElementById('para3')
+
+    para1.style.display = 'block'
+    para2.style.display = 'none'
+    para3.style.display = 'none'
+
+
+}
+
+const hide_para1_3 = () =>{
+    let para1 = document.getElementById('para1')
+    let para2 = document.getElementById('para2')
+    let para3 = document.getElementById('para3')
+
+    para1.style.display = 'none'
+    para2.style.display = 'block'
+    para3.style.display = 'none'
+
+
+}
+
+
+const hide_para1_2 = () =>{
+    let para1 = document.getElementById('para1')
+    let para2 = document.getElementById('para2')
+    let para3 = document.getElementById('para3')
+
+    para1.style.display = 'none'
+    para2.style.display = 'none'
+    para3.style.display = 'block'
+
+
+}
+
